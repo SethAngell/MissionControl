c60daab3d00a4799ff1debbcbc3cc5320c715a5f
SethAngell <accounts@sethangell.com>
2022-06-27 22:55:01 -0400

    A terrible commit of half written code

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,11 +1,36 @@
 # accounts/views.py
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import CustomUserCreationForm
 
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        print(form.errors)
+        if form.is_valid():
+            if form.cleaned_data.get("phone_number")[:2] != "+1":
+                form.cleaned_data[
+                    "phone_number"
+                ] = f'+1{form.cleaned_data["phone_number"]}'
+            user = form.save()
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(request, email=user.email, password=raw_password)
+
+            user.name = form.cleaned_data.get("name")
+            user.save()
+
+            if user is not None:
+                login(request, user)
+            else:
+                print("user is not authenticated")
+            return redirect("home")
+        else:
+            return render(request, "registration/signup.html", {"form": form})
+
+    else:
+        form = CustomUserCreationForm()
+        return render(request, "registration/signup.html", {"form": form})
--- app/polls/models.py
+++ app/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Poll(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+
+
+class Question(models.Model):
+    parent_poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question = models.CharField(max_length=128)
+    subtext = models.CharField(max_length=512)
+    created_at = models.DateTimeField(auto_created=True)
+
+
+class Response(models.Model):
+    pass
